a1ea5bfacf7ff492ac85752a6917215c2c87acff
Orlewilson Maia <orlewilson.maia@gmail.com>
2020-09-24 21:41:54 -0400

    adding Hello World Example

---
+++ 01-Hello-World/src/Main.java
@@ -0,0 +1,20 @@
+/*
+	Disciplina:		Programação Orientada a Objetos
+	Facilitador:	Orlewilson Bentes Maia
+	Data: 			16/09/20
+	Aluno:			escreva seu nome
+	Descrição: 		primeiro programa em Java
+	
+	https://repl.it/languages/java10
+
+*/
+
+// Classe main
+class Main {
+  
+	// método para executar o programa em Java
+	public static void main(String[] args) {
+		//mostrar mensagem no console 
+		System.out.println("Hello world!");
+	}
+}
